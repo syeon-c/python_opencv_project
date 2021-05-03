ae03f4f24017512803391c92735d015c577276cd
YuuJining <yujin092444@gmail.com>
2021-05-03 17:16:29 +0900

    2021/05/03 이미지 유사도 비교/자동화 스크립트 초안 작성

--- image_compare.py
+++ image_compare.py
@@ -1,15 +1,40 @@
 import cv2, numpy as np
 import matplotlib.pylab as plt
+import pyautogui
+from pywinauto.application import Application
+import pywinauto.keyboard as keyboard
+import time
 
-source = cv2.imread(r'C:/Users/gre508/Desktop/GomPlayer_Script/Kaze_Feature_Matching_project/img/Screenshot_1.png')
-img1 = cv2.imread(r'C:/Users/gre508/Desktop/GomPlayer_Script/Kaze_Feature_Matching_project/img/Screenshot_2.png')
-img2 = cv2.imread(r'C:/Users/gre508/Desktop/GomPlayer_Script/Kaze_Feature_Matching_project/img/Screenshot_3.png')
+## 파일 이름 및 시간 설정 변수
+fileName = "sample.avi"
+videoTime = "00:03:00"
+
+## 파일 실행 및 설정
+app = Application(backend="uia").start(r"C:/Program Files (x86)/GRETECH/GOMPlayer/GOM.EXE")
+dig = app.window(title_re=".*곰플레이어")
+
+## 파일 열기
+dig.type_keys('{F2}')
+
+## 검증 파일 재생
+app.파일_열기.Edit13.type_keys(fileName+"{ENTER}")
+
+## 특정 구간 이동 후 화면 캡쳐
+pyautogui.hotkey('g')
+
+time.sleep(3)
+
+dig.Edit.type_keys(videoTime+"{ENTER}")
+pyautogui.hotkey('ctrl','e')
+
+#-- 샘플 이미지 set 불러오기
+sample_source = cv2.imread(r'C:/Users/gre508/Desktop/GomPlayer_Script/Kaze_Feature_Matching_project/img/sample.avi_000120411.png')
+qc_source = cv2.imread(r'C:/Users/gre508/Desktop/GomPlayer_Script/Kaze_Feature_Matching_project/img/Screenshot_1.png')
 
 #-- query 이름으로 사진 띄워줌
-cv2.imshow('query', source)
-cv2.waitKey()
+cv2.imshow('query', sample_source)
 
-imgs = [source, img1, img2]
+imgs = [sample_source, qc_source]
 hists = []
 
 for i, img in enumerate(imgs):
@@ -38,4 +63,3 @@ for j, (name, flag) in enumerate(methods.items()):
         print("img%d:%7.2f"% (i+1, ret), end='\t')
     print()
 plt.show()            
-
--- pywinauto_practice.py
+++ pywinauto_practice.py
@@ -1,24 +1,30 @@
+import pyautogui
 from pywinauto.application import Application
 import pywinauto.mouse as mouse
 import pywinauto.keyboard as keyboard
 import time
 import sys
 
-app = Application(backend="uia").start(r"C:/Program Files (x86)/GRETECH/GOMPlayer/GOM.EXE")
-dig = app.window(title_re=".*곰플레이어")
-##dig.print_control_identifiers()
+def init():
+    app = Application(backend="uia").start(r"C:/Program Files (x86)/GRETECH/GOMPlayer/GOM.EXE")
+    dig = app.window(title_re=".*곰플레이어")
+
+init()
 
 ## 파일 열기
 dig.type_keys('{F2}')
 
 ## 검증 파일 재생
-##app.파일_열기.print_control_identifiers()
 app.파일_열기.Edit13.type_keys("sample.avi{ENTER}")
-##dig.Edit.type_keys('sample.avi{ENTER}')
 
 ## 특정 구간 이동 후 화면 캡쳐
-dig.set_focus()
-keyboard.send_keys('g')
+pyautogui.hotkey('g')
+
+time.sleep(3)
+
+dig.Edit.type_keys("00:02:00{ENTER}")
+pyautogui.hotkey('ctrl','e')
+
 ##dig.print_control_identifiers()
 ## 화면 캡쳐한 파일과 준비해둔 샘플과 이미지 유사도 비교
 
